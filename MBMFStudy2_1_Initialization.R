12ec037e35a22f5f0e12f0ebe46e5f0f4e180a98
FWyckmans <fwyckmans@gmail.com>
2021-01-27 17:56:52 +0100

    Commented the core handler (seems to already be in ts_par7)

--- MBMFStudy2_1_Initialization.R
+++ MBMFStudy2_1_Initialization.R
@@ -27,5 +27,5 @@ library(stringr)
 
 ## Specific
 library(hBayesDM)
-Sys.setenv(BUILD_ALL='true')  # Build all the models on installation
-Sys.setenv(MAKEFLAGS='-j 4')  # Use 4 cores for compilation (or the number you want)
+# Sys.setenv(BUILD_ALL='true')  # Build all the models on installation
+# Sys.setenv(MAKEFLAGS='-j 4')  # Use 4 cores for compilation (or the number you want)
