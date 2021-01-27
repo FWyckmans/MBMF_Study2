2e223d8c8ea7c51f640a4bb17d9d056ddebabb59
FWyckmans <fwyckmans@gmail.com>
2021-01-27 16:43:30 +0100

    First try with my data (does not work yet)

--- MBMFStudy2_4_ComputationWithPackage.R
+++ MBMFStudy2_4_ComputationWithPackage.R
@@ -11,8 +11,8 @@ Playlist = "This is: Johnny Cash by Spotify, Ring of Fire, random"
 d <- read.csv(paste0(Output_path, "ComputationsReady.txt"), sep="")
 
 ########################################## Computations ###########################################
-output <- ts_par7(
-  data = "example", niter = 2000, nwarmup = 1000, nchain = 4, ncore = 4)
+# output <- ts_par7(
+#   data = "example", niter = 2000, nwarmup = 1000, nchain = 4, ncore = 4)
 
 output <- ts_par7(
   data = d, niter = 2000, nwarmup = 1000, nchain = 4, ncore = 4)
