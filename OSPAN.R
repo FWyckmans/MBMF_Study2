47d2e1cb01fd208e4e46f8355462fc1c4cd7dec8
FWyckmans <fwyckmans@gmail.com>
2021-01-27 10:16:57 +0100

    Added script to correct OSPAN

---
+++ OSPAN.R
@@ -0,0 +1,130 @@
+#################################### Initialisation ###############################################
+remove(list = ls())
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+############################################ Parameter ############################################
+Test = 0
+Datapath = "D:/Dropbox/Stage_Labo/OSPAN/"
+Output_path = "D:/Dropbox/Doc_Florent/MB_MF_2/Result_Daw/FinalResults"
+NSInverseOspan = c()
+
+############################################ Frame ################################################
+if (Test != 0){
+  d <- read.delim(paste0(Datapath, Test, ".txt"), header=TRUE, stringsAsFactors=FALSE)
+  NS <- rep(Test, length(d$block.number))
+  Acc <- rep(NA, length(d$block.number))
+  WordAcc <- rep(NA, length(d$block.number))
+  d <- cbind(NS, d, Acc, WordAcc)
+  d <- d%>%
+    filter(block.number > 8)%>%
+    select(NS, Block = block.number, Trial = trial.number, Content = trial.contents, WordResp = set1, WordAcc, RespCorr = correct_operation, Resp = response, Acc, RT)}
+
+if (Test == 0){
+  setwd(Datapath)
+  listeFichiers <- dir(pattern = ".txt")
+  d <- data.frame()
+  for (f in listeFichiers){
+    s <- read.delim(f, header=TRUE, stringsAsFactors=FALSE)
+    NS <- rep(str_sub(f, start = 1, end = -5), length(s$block.number))
+    Acc <- rep(NA, length(s$block.number))
+    WordAcc <- rep(NA, length(s$block.number))
+    s <- cbind(NS, s, Acc, WordAcc)
+    d <- rbind(d, s)}
+  d <- d%>%
+    filter(block.number > 8)%>%
+    select(NS, Block = block.number, Trial = trial.number, Content = trial.contents, WordResp = set1, WordAcc, RespCorr = correct_operation, Resp = response, Acc, RT)}
+
+setwd(Output_path)
+
+########################################## Modify frame ###########################################
+ComputeAcc <- function(d){     # Fonction pour calculer les réponses correctes
+  if (d$NS %in% NSInverseOspan){  
+    d$Acc[d$Resp == "correct" & d$RespCorr == 1] <- 0
+    d$Acc[d$Resp == "incorrect" & d$RespCorr == 0] <- 0
+    d$Acc[d$Resp == "correct" & d$RespCorr == 0] <- 1
+    d$Acc[d$Resp == "incorrect" & d$RespCorr == 1] <- 1}
+  
+  else{  
+    d$Acc[d$Resp == "correct" & d$RespCorr == 1] <- 1
+    d$Acc[d$Resp == "incorrect" & d$RespCorr == 0] <- 1
+    d$Acc[d$Resp == "correct" & d$RespCorr == 0] <- 0
+    d$Acc[d$Resp == "incorrect" & d$RespCorr == 1] <- 0}
+  return(d)
+}
+
+WordClean <- function(d){
+  unwanted_array = list(    'S'='S', 's'='s', 'Z'='Z', 'z'='z', 'À'='A', 'Á'='A', 'Â'='A', 'Ã'='A', 'Ä'='A', 'Å'='A', 'Æ'='A', 'Ç'='C', 'È'='E', 'É'='E',
+                            'Ê'='E', 'Ë'='E', 'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I', 'Ñ'='N', 'Ò'='O', 'Ó'='O', 'Ô'='O', 'Õ'='O', 'Ö'='O', 'Ø'='O', 'Ù'='U',
+                            'Ú'='U', 'Û'='U', 'Ü'='U', 'Ý'='Y', 'Þ'='B', 'ß'='Ss', 'à'='a', 'á'='a', 'â'='a', 'ã'='a', 'ä'='a', 'å'='a', 'æ'='a', 'ç'='c',
+                            'è'='e', 'é'='e', 'ê'='e', 'ë'='e', 'ì'='i', 'í'='i', 'î'='i', 'ï'='i', 'ð'='o', 'ñ'='n', 'ò'='o', 'ó'='o', 'ô'='o', 'õ'='o',
+                            'ö'='o', 'ø'='o', 'ù'='u', 'ú'='u', 'û'='u', 'ý'='y', 'ý'='y', 'þ'='b', 'ÿ'='y' )
+  d$WordResp[d$WordResp==1|d$WordResp==0] <- NA
+  d$WordResp <- str_remove_all(d$WordResp, "[²-]")
+  for (i in c(1:length(d$NS))){
+    d$Word[i] <- chartr(paste(names(unwanted_array), collapse=''),
+                        paste(unwanted_array, collapse=''),
+                        d$Word[i])
+    d$WordResp[i] <- chartr(paste(names(unwanted_array), collapse=''),
+                        paste(unwanted_array, collapse=''),
+                        d$WordResp[i])
+  }
+  return(d)
+}
+
+WordRespCol <- function(d){
+  dT <- data.frame()
+  for (i in unique(d$Block[d$Block %% 2 == 1])){
+    dt <- filter(d, (d$Block == i|d$Block == i+1))
+    for (l in c(1:max(dt$Trial))){
+      dt$WordResp[l] <- dt$WordResp[l+max(dt$Trial)]
+    }
+    dt <- filter(dt, Block%%2 == 1)
+    dT <- rbind(dT, dt)
+  }
+  return(dT)}
+
+CorrectWord <- function(d){
+  d$WordAcc[tolower(str_remove_all(d$Word, "[ ]")) == tolower(str_remove_all(d$WordResp, "[ ]"))] <- 1
+  d$WordAcc[tolower(str_remove_all(d$Word, "[ ]")) != tolower(str_remove_all(d$WordResp, "[ ]"))] <- 0
+  return(d)
+}
+
+dT <- data.frame()
+for (i in unique(d$NS)){
+  print(i)
+  dt <- filter(d, NS == i) # Get frame for participant
+  
+  dt <- ComputeAcc(dt) # Compute the accuracy for each trial
+  dt <- separate(dt, Content, into = c("Calc", "Word"), sep = " ?, ")
+  dt <- WordClean(dt) # Word = NA for calcul, remove accents and strange symbols
+  dt$WordResp[dt$WordResp == " "] <- "NoResp"  # Code forgotten words
+  dt <- WordRespCol(dt) # Create a col with the word written
+  dt <- CorrectWord(dt) # Check if the word is correct
+  dT <- rbind(dT, dt) # Final tab
+}
+
+d <- dT%>%
+  select(NS, Block, Trial, Word, WordResp, WordAcc, Acc, RT)%>%
+  group_by(NS, Block)%>%
+  summarise(nWordAcc = sum(WordAcc, na.rm = T), WordAcc = mean(WordAcc, na.rm = T), Acc = mean(Acc, na.rm = T), RT = mean(RT, na.rm = T),
+            nTrial = length(Trial))%>%
+  group_by()%>%
+  select(NS, WordAcc, nWordAcc, nTrial, Acc, RT)
+
+######################################### Compute score ###########################################
+CalcOK <- rep(0, length(d$NS))
+d <- cbind(d, CalcOK)
+d$CalcOK[d$Acc>0.79] <- 1
+
+d$nWordAcc[d$CalcOK==0] <- 0
+
+dOspan <- d%>%
+  # filter(CalcOK == 1)%>%
+  group_by(NS)%>%
+  summarise(nWord = sum(nWordAcc, na.rm = T), RT = mean(RT, na.rm = T))
+
+############################################# Export ##############################################
+write.table(dOspan, paste0(Output_path, "/dOSPAN.txt"), row.names = F, col.names = T, dec = ".", sep = "\t")
