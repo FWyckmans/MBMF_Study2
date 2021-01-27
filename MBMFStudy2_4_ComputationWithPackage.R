1419a52cded4387f7f9b99fd454a79385b0c5964
FWyckmans <fwyckmans@gmail.com>
2021-01-27 15:54:25 +0100

    Misc

--- MBMFStudy2_4_ComputationWithPackage.R
+++ MBMFStudy2_4_ComputationWithPackage.R
@@ -5,7 +5,7 @@ Test = 0
 Way = 'Wyck'
 Datapath = "Raw_Data/RLTaskData/"
 Output_path = "Output/"
-Playlist = "This is Elton John by Spotify, Benny and the Jets"
+Playlist = "This is Johnny Cash by Spotify, Ring of Fire, random"
 
 ############################################# Frame ###############################################
 d <- read.csv(paste0(Output_path, "ComputationsReady.txt"), sep="")
