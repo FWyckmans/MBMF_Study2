db185a657e670ce35b30272ea947743443256cd7
FWyckmans <fwyckmans@gmail.com>
2021-01-28 09:55:54 +0100

    Added a test support

--- MBMFStudy2_4_ComputationWithPackage.R
+++ MBMFStudy2_4_ComputationWithPackage.R
@@ -2,13 +2,16 @@ remove(list = ls())
 
 ############################################ Parameter ############################################
 Test = 0
-Way = 'Wyck'
 Datapath = "Raw_Data/RLTaskData/"
 Output_path = "Output/"
 Playlist = "This is: Johnny Cash by Spotify, Ring of Fire, random"
 
 ############################################# Frame ###############################################
 d <- read.csv(paste0(Output_path, "ComputationsReady.txt"), sep="")
+
+if (Test != 0){
+  d <- filter(d, subjID == Test)
+}
 
 ########################################## Computations ###########################################
 # output <- ts_par7(
