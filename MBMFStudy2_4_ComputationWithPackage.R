ac2ad1c928a0c8f390a11a3c5e692bc5ba57c135
FWyckmans <fwyckmans@gmail.com>
2021-01-28 13:57:01 +0100

    Added a way to start fresh or only with new data

--- MBMFStudy2_4_ComputationWithPackage.R
+++ MBMFStudy2_4_ComputationWithPackage.R
@@ -2,7 +2,7 @@ remove(list = ls())
 
 ############################################ Parameter ############################################
 Test = 0
-StartOver = 0
+StartOver = 0 # 1 to start from scratch, 0 to start with only new participants
 Datapath = "Raw_Data/RLTaskData/"
 Output_path = "Output/"
 Playlist = "This is: Blondie by Spotify, Call me, Random"
@@ -14,6 +14,11 @@ if (Test != 0){
   d <- filter(d, subjID == Test)
 }
 
+if (StartOver == 0){
+  ComputationParameter <- read.delim(paste0(Output_path, "ComputationParameter.txt"))
+  d <- filter(d, !subjID %in% ComputationParameter$subjID)
+}
+
 ########################################## Computations ###########################################
 # output <- ts_par7(
 #   data = "example", niter = 2000, nwarmup = 1000, nchain = 4, ncore = 4)
@@ -23,5 +28,9 @@ output <- ts_par7(
 
 ########################################## Save Output ###########################################
 dOutput <- output$allIndPars
+if (StartOver == 0){
+  dOutput <- rbind(ComputationParameter, output$allIndPars)
+}
+
 if (Test == 0){
   write.table(dOutput, paste0(Output_path, "ComputationParameter.txt"), col.names = T, row.names = F, sep = "\t", dec = ".")}
