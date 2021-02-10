ff4cae004855880e6b39a61b30d55195e66fe682
FWyckmans <fwyckmans@gmail.com>
2021-02-10 17:01:35 +0100

    Add a script for additionnal computations (mean and reglog). Mean computations are ready (and written). RegLog to do.

---
+++ MBMFStudy2_5_CompMeanAndRegLog.R
@@ -0,0 +1,41 @@
+remove(list = ls())
+source("MBMFStudy2_1_Initialization.R")
+
+############################################ Parameter ############################################
+Test = 0
+Way = 'Wyck'
+Datapath = "Raw_Data/RLTaskData/"
+Output_path = "Output/"
+
+############################################ Frame ################################################
+d <- read.csv(paste0(Output_path, "/ComputationsReady.txt"), sep = " ")
+
+##### Recode PrReward
+d$PrReward[d$PrReward==0] <- "Unrewarded"
+d$PrReward[d$PrReward==1] <- "Rewarded"
+
+##### Recode PrTransition
+d$PrTransition[d$PrTransition==-1] <- "Rare"
+d$PrTransition[d$PrTransition==1] <- "Common"
+
+###################################### Features engineering #######################################
+##### Mean
+## Long format
+dProbaLong <- d%>%
+  group_by(subjID, PrReward, PrTransition)%>%
+  summarise(Proba = mean(Stay))%>%
+  unite("Type", c(PrReward, PrTransition), sep = " ")%>%
+  group_by()
+
+dProbaLong$Type[dProbaLong$Type=="Rewarded Common"] <- "PRCw"
+dProbaLong$Type[dProbaLong$Type=="Rewarded Rare"] <- "PRRw"
+dProbaLong$Type[dProbaLong$Type=="Unrewarded Common"] <- "PUCw"
+dProbaLong$Type[dProbaLong$Type=="Unrewarded Rare"] <- "PURw"
+
+## Large format
+dProbaLarge <- dProbaLong%>%
+  spread(key = Type, value = Proba)
+
+########################################## Save Output ###########################################
+write.table(dProbaLarge, paste0(Output_path, "ProbaLarge.txt"), sep = "\t", dec = ".")
+write.table(dProbaLong, paste0(Output_path, "ProbaLong.txt"), sep = "\t", dec = ".")
