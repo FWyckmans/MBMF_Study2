75e3c14b9e22c785b259906c48f6ddcc9c29c5fb
FWyckmans <fwyckmans@gmail.com>
2021-02-12 14:35:11 +0100

    Creation of a data preparation hyperscript

---
+++ DataPreparationHS.R
@@ -0,0 +1,9 @@
+remove(list = ls())
+
+############################################ Parameter ############################################
+source("DataPrep/MBMFStudy2_1_Initialization.R")
+source("DataPrep/MBMFStudy2_2_OSPAN.R")
+source("DataPrep/MBMFStudy2_3_RLTask.R")
+source("DataPrep/MBMFStudy2_4_ComputationWithPackage.R")
+source("DataPrep/MBMFStudy2_5_CompMeanAndRegLog.R")
+source("DataPrep/MBMFStudy2_6_ClinicalData.R")
