e2e42e834639ff085448758376613159da3a55d9
FWyckmans <fwyckmans@gmail.com>
2021-02-12 11:07:42 +0100

    Prepared to add specific missing

--- AdditionalScripts/MissingCortisol.R
+++ AdditionalScripts/MissingCortisol.R
@@ -8,15 +8,17 @@ Output_path = "Output/"
 ############################################ Frame ################################################
 dCort <- read.delim(paste0(Output_path,"dTot.txt"))%>%
   filter(OKd == 1)%>%
-  select(NS, Initiales, Analyse_1, Analyse_2, Analyse_3, Analyse_4)
+  select(NS, Initiales, Condition, Analyse_1, Analyse_2, Analyse_3, Analyse_4)
 
+dExp <- select(dCort, NS, Initiales, Analyse_1, Analyse_2, Analyse_3, Analyse_4)
 
 ############################################# Export ##############################################
-write_xlsx(dCort, paste0(Output_path, "Cortisol.xlsx"))
+write_xlsx(dExp, paste0(Output_path, "Cortisol.xlsx"))
 
 ########################################### Descriptive ###########################################
+########## Total missing
 nDone <- sum(!is.na(dCort$Analyse_1))
 nMissing <- sum(is.na(dCort$Analyse_1))
 nTotal <- nDone + nMissing
-
-cat(paste0("You already have ", nDone, " analyses\n", nMissing, " analyses are missing\nFor a total of ", nTotal, " analyses"))
+TextTot <- paste0("You already have ", nDone, " analyses\n", nMissing, " analyses are missing\nFor a total of ", nTotal, " analyses")
+cat(TextTot)
