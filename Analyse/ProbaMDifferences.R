aeab94e9c21b07d0d45e9983b3ad7fe653953a59
FWyckmans <fwyckmans@gmail.com>
2021-03-12 20:29:03 +0100

    Started classical plot for MB MF illustration. Still have to add error bar

--- Analyse/ProbaMDifferences.R
+++ Analyse/ProbaMDifferences.R
@@ -8,6 +8,7 @@ Output_path = "Output/"
 ############################################# Frame ###############################################
 d <- read.delim(paste0(Output_path,"dTot.txt"))%>%
   filter(OKd == 1)%>%
+  filter(!is.na(StressGr))%>%
   select(subjID, Condition, Sample, StressGr, StressGrM, StressGrSR, StressGrSRM,
          PRCw, PRRw, PUCw, PURw, PRCd, PRRd, PUCd, PURd)%>%
   mutate(MBw = PRCw - PRRw - PUCw + PURw, MFw = PRCw + PRRw - PUCw - PURw,
@@ -26,3 +27,40 @@ boxplot(MBw ~ StressGr*Sample, data = d)
 boxplot(MBw ~ StressGrM*Sample, data = d)
 
 boxplot(MBw ~ StressGrSR*Sample, data = d)
+
+##### Classic Graphic
+dG <- d%>%
+  select(subjID, Condition, Sample, StressGr, StressGrM, StressGrSR, StressGrSRM,
+         PRCw, PRRw, PUCw, PURw, PRCd, PRRd, PUCd, PURd)%>%
+  gather(key = "Score", value = "Value", PRCw:PURd)
+dG$StressGrM[dG$StressGrM == 1] <- "Stressed"
+dG$StressGrM[dG$StressGrM == -1] <- "Not Stressed"
+
+
+ClassicGraph <- function(d, Method = "Mine"){
+  if (Method == "Mine"){
+    dI <- filter(d, Score %in% c("PRCw", "PRRw", "PUCw", "PURw"))
+  }
+  
+  if (Method == "Daw"){
+    dI <- filter(d, Score %in% c("PRCd", "PRRd", "PUCd", "PURd"))
+  }
+  
+  dI <- dI%>%
+    group_by(Sample, StressGrM, Score)%>%
+    summarise(M = mean(Value, na.rm = T), SD = sd(Value, na.rm = T), n = length(Value))
+  
+  for (j in c("Stressed", "Not Stressed")){  
+    di <- filter(dI, StressGrM == j)
+    for (i in c("HC", "Gambler", "Alc")) {
+      di2 <- filter(di, Sample == i)
+    
+      Plot <- ggplot(data = di2, aes(x = Score, y = M)) +
+        geom_bar(stat = "identity") +
+        ggtitle(paste0(i, " ", j))
+      print(Plot)
+    }
+  }
+}
+
+ClassicGraph(dG)
