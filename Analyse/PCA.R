73a2a831ac611416739e5d3de14573a149edf165
FWyckmans <fwyckmans@gmail.com>
2021-03-18 15:10:45 +0100

    First PCA to prepare procedure

--- Analyse/PCA.R
+++ Analyse/PCA.R
@@ -17,12 +17,12 @@ PCA = c("SOGS", "AUDIT", "Fagerstrom",
 
 
 
-PCA = c("SOGS", "AUDIT", "Fagerstrom",
+AllCol$PCA = c("SOGS", "AUDIT", "Fagerstrom",
         "Beck", "PANASPos", "PANASNeg", "SCL90R",
         "STAIA", "STAIB", "SRRS", "PunitionSens", "RewardSens",
         "UPPS_Total", "NegUr", "PosUr", "LackOfPrem", "LackOfPers", "Sensation")
 
-pca <- PCA(d[AllCol$PCA], scale.unit = TRUE, ncp = 5, graph = TRUE)
+pca <- PCA(d[AllCol$PCA], scale.unit = TRUE, ncp = 3, graph = TRUE)
 fviz_eig(pca, addlabels = TRUE)
 
 var <- get_pca_var(pca)
