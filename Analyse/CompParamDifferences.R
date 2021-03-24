eaa243297a353ce3ff55ec1ac9b0da8cf83e077c
FWyckmans <fwyckmans@gmail.com>
2021-03-24 19:41:47 +0100

    misc

--- Analyse/CompParamDifferences.R
+++ Analyse/CompParamDifferences.R
@@ -15,3 +15,21 @@ d <- read.delim(paste0(Output_path,"dTot.txt"))%>%
 ########## Parameter = w
 ##### Main ANOVA
 MainAOV(d, VoI = "lambda", Groups = c("Sample", "StressGrM"), RemoveOutTechnique = "MAD")
+
+# bp(d, "w", "Condition")
+# bp(d, "w", "Sample")
+# bp(d, "w", c("Sample", "StressGr"))
+# bp(d, "w", c("Sample", "StressGrM"))
+# bp(d, "w", c("Sample", "StressGrSR"))
+
+##### Parameter = lambda
+# bp(d, "lambda", "Condition")
+# bp(d, "lambda", "Sample")
+# bp(d, "lambda", c("Sample", "StressGr"))
+# bp(d, "lambda", c("Sample", "StressGrM"))
+# bp(d, "lambda", c("Sample", "StressGrSR"))
+# bp(d, "lambda", c("Sample", "StressGrSRM"))
+
+# dA <- OutliersModif(d, c("a1", "beta1", "a2", "beta2", "pi", "w", "lambda"), Groups = "Sample")
+# dA <- OutliersModif(d, c("a1", "beta1", "a2", "beta2", "pi", "w", "lambda"), Groups = "Condition")
+
