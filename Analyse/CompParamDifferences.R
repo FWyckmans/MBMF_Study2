fbbd6f3693747b03c1df19392a31f4eeedc309e5
FWyckmans <fwyckmans@gmail.com>
2021-03-24 19:41:30 +0100

    Create a function to compare parameters. Still has to modify the output a bit

--- Analyse/CompParamDifferences.R
+++ Analyse/CompParamDifferences.R
@@ -12,26 +12,6 @@ d <- read.delim(paste0(Output_path,"dTot.txt"))%>%
          a1, beta1, a2, beta2, pi, w, lambda)
 
 ############################################ Graphics ##############################################
-##### Parameter = w
-bp(d, "w", "Condition", F)
-bp(d, "w", "Sample")
-bp(d, "w", c("Sample", "StressGr"))
-bp(d, "w", c("Sample", "StressGrM"))
-bp(d, "w", c("Sample", "StressGrSR"))
-# bp(d, "w", c("Sample", "StressGrSRM"))
-
-##### Parameter = lambda
-bp(d, "lambda", "Condition")
-bp(d, "lambda", "Sample")
-bp(d, "lambda", c("Sample", "StressGr"))
-bp(d, "lambda", c("Sample", "StressGrM"))
-bp(d, "lambda", c("Sample", "StressGrSR"))
-# bp(d, "lambda", c("Sample", "StressGrSRM"))
-
-d <- OutliersModif(d, "w", Groups = "Sample")
-
-dDescr <- DescrFrame(d, Btwn = "Sample")
-dDescr <- DescrFrame(d, Btwn = "Condition")
-anova <- aov(w ~ Sample, data = d)
-summary(anova)
-t.test(d$w[d$Sample=="Alc"], d$w[d$Sample=="HC"])
+########## Parameter = w
+##### Main ANOVA
+MainAOV(d, VoI = "lambda", Groups = c("Sample", "StressGrM"), RemoveOutTechnique = "MAD")
