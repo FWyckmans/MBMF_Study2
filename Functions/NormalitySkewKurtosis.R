b3c2c204b2ab5972db53536516b8e02f5012d816
FWyckmans <fwyckmans@gmail.com>
2021-08-12 14:56:48 +0200

    Updated function

--- Functions/NormalitySkewKurtosis.R
+++ Functions/NormalitySkewKurtosis.R
@@ -1,12 +1,18 @@
-NormalitySkewKurtosis <- function(d, VoI, Groups) {
+NormalitySkewKurtosis <- function(d, VoI, Groups, Format = "Large") {
   # Give the dataframe, the index of the Variable of Interest and the index of the group column
   # Indicate if you want a between-subject comparison
+  if (!(Format %in% c("Large", "Long"))){
+    stop("Unsupported Format")
+  }
   
+  cn <- colnames(d)
+  ##### Needed packages
   if(!require(dplyr)){install.packages('dplyr')}
   library(dplyr)
   if(!require(tidyr)){install.packages('tidyr')}
   library(tidyr)
   
+  ##### Needed functions
   spssSkewKurtosis=function(x) {
     w=length(x)
     m1=mean(x, na.rm = T)
@@ -23,7 +29,8 @@ NormalitySkewKurtosis <- function(d, VoI, Groups) {
     return(mat)
   }
   
-  dT[[Groups]] <- factor(dT[[Groups]], levels = unique(dT[[Groups]]), ordered = F)
+  ##### Main code
+  d[[Groups]] <- factor(d[[Groups]], levels = unique(d[[Groups]]), ordered = F)
   
   Gr = unique(d[[Groups]])
   dT <- data.frame()
@@ -33,25 +40,61 @@ NormalitySkewKurtosis <- function(d, VoI, Groups) {
     n = 1
     for (i in VoI){
       vect <- dt[[i]]
-      moy <- mean(vect, na.rm = T)
-      ET <- sd(vect, na.rm = T)
-      mdn <- median(vect, na.rm = T)
-      dTemp[n,1] = colnames(dt[i], do.NULL = TRUE, prefix = "col")
-      dTemp[n,2] = Gr[g]
-      dTemp[n,3] = round(moy,digits=2)
-      dTemp[n,4] = round(ET,digits=2)
-      dTemp[n,5] = round(mdn, digits=2)
-      dTemp[n,6] = round(spssSkewKurtosis(vect)[2]/spssSkewKurtosis(vect)[4],digits=5)
-      dTemp[n,7] = round(spssSkewKurtosis(vect)[1]/spssSkewKurtosis(vect)[3],digits=5)
-      dTemp[n,8] = round(spssSkewKurtosis(vect)[2],digits=5)
-      dTemp[n,9] = round(spssSkewKurtosis(vect)[1],digits=5)
-      dTemp[n,10] <- 'OK'
-      dTemp[n,11] <- length(vect)
-      if ((dTemp[n,6] < -1.96 | dTemp[n,6] > 1.96) | (dTemp[n,7] < -1.96 | dTemp[n,7] > 1.96)){
-        dTemp[n,10] <- "Not_OK"
+      
+      # Handle numeric
+      if (!(is.character(vect) | is.factor(vect))){
+        moy <- mean(vect, na.rm = T)
+        ET <- sd(vect, na.rm = T)
+        mdn <- median(vect, na.rm = T)
+        dTemp[n,1] = colnames(dt[i], do.NULL = TRUE, prefix = "col")
+        dTemp[n,2] = Gr[g]
+        dTemp[n,3] = round(moy,digits=2)
+        dTemp[n,4] = round(ET,digits=2)
+        dTemp[n,5] = round(mdn, digits=2)
+        dTemp[n,6] = round(spssSkewKurtosis(vect)[2]/spssSkewKurtosis(vect)[4],digits=5)
+        dTemp[n,7] = round(spssSkewKurtosis(vect)[1]/spssSkewKurtosis(vect)[3],digits=5)
+        dTemp[n,8] = round(spssSkewKurtosis(vect)[2],digits=5)
+        dTemp[n,9] = round(spssSkewKurtosis(vect)[1],digits=5)
+        dTemp[n,10] <- 'OK'
+        dTemp[n,11] <- length(vect)
+        if ((dTemp[n,6] < -1.96 | dTemp[n,6] > 1.96) | (dTemp[n,7] < -1.96 | dTemp[n,7] > 1.96)){
+          dTemp[n,10] <- "Not_OK"
+        }
+        n = n+1
       }
-      n = n+1
+      
+      # Handle character
+      if (is.character(vect) | is.factor(vect)){
+        nByGroups <- function(v){
+          if (!is.factor(v)){
+            v <- as.factor(v)
+          }
+          levelname <- levels(v)
+          nl <- length(levelname)
+          n = length(v[!is.na(v)])
+          nbylevel <- ""
+          for (i in levelname) {
+            output <- paste0(i, " (n = ", length(v[(v==i & !(is.na(v)))]) ,")")
+            nbylevel <- substring(paste(nbylevel, output, sep = " | "), 2)
+          }
+          return(nbylevel)
+        }
+        
+        dTemp[n,1] = colnames(dt[i], do.NULL = TRUE, prefix = "col")
+        dTemp[n,2] = Gr[g]
+        dTemp[n,3] = NA
+        dTemp[n,4] = NA
+        dTemp[n,5] = NA
+        dTemp[n,6] = NA
+        dTemp[n,7] = NA
+        dTemp[n,8] = NA
+        dTemp[n,9] = NA
+        dTemp[n,10] <- "Not_OK"
+        dTemp[n,11] <- nByGroups(vect)
+        n = n+1
+      }
     }
+    
     dT <- rbind(dT, dTemp)
     
   }
@@ -60,7 +103,7 @@ NormalitySkewKurtosis <- function(d, VoI, Groups) {
                    KurtosisBYSD = V6, SkewnessBYSD = V7, Kurtosis = V8, Skewness = V9,
                    Normality = V10, N = V11)
   
-  # Evaluate if normality ok in each group
+  # Evaluate if normality is ok in each group
   AllNorm <- rep("OK", length(dDescr$Variable))
   dDescr <- cbind(dDescr, AllNorm)
   dDescr$AllNorm <- as.character(dDescr$AllNorm)
@@ -68,79 +111,175 @@ NormalitySkewKurtosis <- function(d, VoI, Groups) {
     if ("Not_OK" %in% dDescr$Normality[dDescr$Variable == i]){
       dDescr$AllNorm[dDescr$Variable == i] <- "Not_OK"}
   }
-  dDescr <- arrange(dDescr, Variable)
-  return(dDescr)
+  
+  dDescrLarge <- arrange(dDescr, Variable)
+  # dDescrLarge
+  if (Format == "Large"){
+    return(dDescrLarge)}
+  
+  if (Format == "Long"){
+    dMean <- dT%>%
+      select(c(1, 2, 3))%>%
+      spread(key = 2, value = 3)
+    dSD <- dT%>%
+      select(c(1, 2, 4))%>%
+      spread(key = 2, value = 3)
+    dN <- dT%>%
+      select(c(1, 2, 11))%>%
+      spread(key = 2, value = 3)
+    dMedian <- dT%>%
+      select(c(1, 2, 5))%>%
+      spread(key = 2, value = 3)
+    
+    dDescr2 <- cbind(dMean, dSD[,c(2:3)], dN[,c(2:3)])
+    colnames(dDescr2) <- c("Variable", paste0("Mean_", colnames(dDescr2)[2]), paste0("Mean_", colnames(dDescr2)[3]),
+                           paste0("SD_", colnames(dDescr2)[4]), paste0("SD_", colnames(dDescr2)[5]),
+                           paste0("N_", colnames(dDescr2)[6]), paste0("N_", colnames(dDescr2)[7]))
+    # return(dDescr2)
+    
+    
+    # Creation of dDescrLong
+    Variable <- dDescr2$Variable
+    Test <- rep("", length(Variable))
+    dDescrLong <- data.frame(Variable, Test)
+    # G = unique(dDescr$Group)
+    
+    for (i in 1:g) {
+      Value <- rep("", length(Variable))
+      dDescrLong <- cbind(dDescrLong, Value)
+      n = sum(d[Groups]==levels(Gr)[i])
+      colnames(dDescrLong)[length(dDescrLong)] <- paste0(levels(Gr)[i], " (n = ", n, ")")
+    }
+    dDescrLong <- dDescrLong[, c(1, 3:length(dDescrLong), 2)]
+    
+    for (i in dDescr2$Variable) {
+      if (g == 2){
+        if ("OK" %in% dDescr$Normality[dDescr$Variable == i]){
+          # Display mean (SD) | Median
+          compt = 2
+          for (grpe in levels(Gr)) {
+            M = sum(subset(dMean, V1 == i)[grpe])
+            sd = sum(subset(dSD, V1 == i)[grpe])
+            Mdn = sum(subset(dMedian, V1 == i)[grpe])
+            
+            Display <- paste0(M, " (", sd, ") | Mdn = ", Mdn)
+            
+            dDescrLong[dDescrLong$Variable == i, compt] <- Display
+            
+            compt = compt+1
+          }
+          
+          # Test difference
+          Test <- t.test(d[i][d[Groups]==levels(Gr)[1]], d[i][d[Groups]==levels(Gr)[2]])
+          ddl = round(Test$parameter, digits = 2)
+          t = round(abs(Test$statistic), digits = 2)
+          p = round(Test$p.value, digits = 3)
+          
+          if (p >= 0.05){
+            Display <- paste0("t(", ddl, ") = ", t, ", p = ", p) #, ", d = ", "d")
+          }
+          
+          if ((p > 0) & (p < 0.05)){
+            Display <- paste0("t(", ddl, ") = ", t, ", p = ", p, "*") #, ", d = ", "d")
+          }
+          
+          if (p == 0){
+            Display <- paste0("t(", ddl, ") = ", t, ", p < 0.001*") #, ", d = ", "d")
+          }
+          
+          if (p == 1){
+            Display <- paste0("t(", ddl, ") = ", t, ", p > 0.99") #, ", d = ", "d")
+          }
+          
+          dDescrLong$Test[dDescrLong$Variable == i] <- Display
+        }
+        
+        if (("Not_OK" %in% dDescr$Normality[dDescr$Variable == i]) & (F %in% is.na(dDescr$Mean[dDescr$Variable == i]))){
+          # Display mean (SD) | Median
+          compt = 2
+          for (grpe in levels(Gr)) {
+            M = sum(subset(dMean, V1 == i)[grpe])
+            sd = sum(subset(dSD, V1 == i)[grpe])
+            Mdn = sum(subset(dMedian, V1 == i)[grpe])
+            Display <- paste0(M, " (", sd, ") | Mdn = ", Mdn)
+            dDescrLong[dDescrLong$Variable == i, compt] <- Display
+            compt = compt+1
+          }
+          
+          # Test difference
+          Test <- wilcox.test(d[i][d[Groups]==levels(Gr)[1]], d[i][d[Groups]==levels(Gr)[2]])
+          w = round(Test$statistic, digits = 2)
+          p = round(Test$p.value, digits = 3)
+          nG1 = length(d[i][(d[Groups]==levels(Gr)[1] & !is.na(d[i][d[Groups]==levels(Gr)[1]]))])
+          nG2 = length(d[i][(d[Groups]==levels(Gr)[2] & !is.na(d[i][d[Groups]==levels(Gr)[2]]))])
+          ddl = nG1 + nG2
+          # ddl = sum(as.numeric(dDescr$N[dDescr$Variable == i]))
+          
+          # length(v[(v==i & !(is.na(v)))])
+          
+          if (p >= 0.05){
+            Display <- paste0("U(", ddl, ") = ", w, ", p = ", p)
+          }
+          
+          if ((p > 0) & (p < 0.05)){
+            Display <- paste0("U(", ddl, ") = ", w, ", p = ", p, "*")
+          }
+          
+          if (p == 0){
+            Display <- paste0("U(", ddl, ") = ", w, ", p < 0.001*")
+          }
+          
+          if (p == 1){
+            Display <- paste0("U(", ddl, ") = ", w, ", p > 0.99 ")
+          }
+          
+          dDescrLong$Test[dDescrLong$Variable == i] <- Display
+        }
+        
+        if (("Not_OK" %in% dDescr$Normality[dDescr$Variable == i]) & (T %in% is.na(dDescr$Mean[dDescr$Variable == i]))){
+          # Display N (...) | N (...)
+          compt = 2
+          for (grpe in levels(Gr)) {
+            
+            
+            Display <- as.character(subset(dN,V1 == i)[grpe])
+            dDescrLong[dDescrLong$Variable == i, compt] <- Display
+            compt = compt+1
+          }
+          
+          # Test difference
+          Test <- chisq.test(d[[i]], d[[Groups]])
+          X2 = round(Test$statistic, digits = 2)
+          p = round(Test$p.value, digits = 3)
+          ddl = round(Test$parameter, digits = 3)
+          
+          if (p >= 0.05){
+            Display <- paste0("X2(", ddl, ") = ", X2, ", p = ", p)
+          }
+          
+          if ((p > 0) & (p < 0.05)){
+            Display <- paste0("X2(", ddl, ") = ", X2, ", p = ", p, "*")
+          }
+          
+          if (p == 0){
+            Display <- paste0("X2(", ddl, ") = ", X2, ", p < 0.001 ")
+          }
+          
+          if (p == 1){
+            Display <- paste0("X2(", ddl, ") = ", X2, ", p > 0.99 ")
+          }
+          
+          dDescrLong$Test[dDescrLong$Variable == i] <- Display
+        }
+      }
+    }
+    # x <- colnames(d)
+    dDescrLong <- dDescrLong %>%
+      slice(match(cn, Variable))
+    
+    # dDescr %>%
+    #   slice(match(x, Variable))
+    
+    return(dDescrLong)
+  }
 }
-#   dMean <- dT%>%
-#     select(c(1, 2, 3))%>%
-#     spread(key = 2, value = 3)
-#   dSD <- dT%>%
-#     select(c(1, 2, 4))%>%
-#     spread(key = 2, value = 3)
-#   dN <- dT%>%
-#     select(c(1, 2, 11))%>%
-#     spread(key = 2, value = 3)
-#   
-#   dDescr2 <- cbind(dMean, dSD[,c(2:3)], dN[,c(2:3)])
-#   colnames(dDescr2) <- c("Variable", paste0("Mean_", colnames(dDescr2)[2]), paste0("Mean_", colnames(dDescr2)[3]),
-#                          paste0("SD_", colnames(dDescr2)[4]), paste0("SD_", colnames(dDescr2)[5]),
-#                          paste0("N_", colnames(dDescr2)[6]), paste0("N_", colnames(dDescr2)[7]))
-#   
-############ Until here things are fine then shit hits the fans
-#   # Create nice frame
-#   dDescr3 <- dDescr2
-#   n = 0
-#   for (g in c(2:((length(dDescr2)+1)/2))) {
-#     NewCol <- rep(0, length(dDescr2$V1))
-#     dDescr3 <- cbind(dDescr3, NewCol)
-#     for (i in c(1:length(dDescr3$V1))){
-#       dDescr3[i, ncol(dDescr3)] <- paste0(dDescr2[i,g], " (", dDescr2[i,g+2], ")")
-#     }
-#     new_col_names <- paste0(" (n = ", dDescr$N[dDescr$Group == dDescr$Group[g]][1], ")")
-#     names(dDescr3)[ncol(dDescr3)] <- paste0(names(dDescr3)[g], new_col_names, sep = "\n")
-#     n <- n+1
-#   }
-#   
-#   names(dDescr3)[1] <- "Variable"
-#   dDescr3 <- dDescr3[ -c(2:(g+n)) ]
-#   
-#   if ((Btwn == TRUE) & (length(unique(d[[Groups]] == 2)))){
-#     NewCol <- rep(0, length(dDescr3$Variable))
-#     dDescr3 <- cbind(dDescr3, NewCol)
-#     
-#     for (i in c(1:length(VoI))) {
-#       # print(i)
-#       Gr1 <- filter(d, d[[Groups]] == Gr[1])
-#       Gr2 <- filter(d, d[[Groups]] == Gr[2])
-#       
-#       ColName <- names(d[VoI[i]])
-#       
-#       if (dDescr$AllNorm[dDescr$Variable==ColName][1] == "OK"){
-#         
-#         Comp <- t.test(Gr1[grep(paste0("^", ColName, "$"), colnames(Gr1))],
-#                        Gr2[grep(paste0("^", ColName, "$"), colnames(Gr2))])
-#         t = round(as.numeric(Comp[1]), digits = 2)
-#         df = round(as.numeric(Comp[2]), digits = 2)
-#         p = round(as.numeric(Comp[3]), digits = 5)
-#         dDescr3[i,(length(VoI)+2)] <- paste0('t(', df, ') = ', t, ", p = ", p)
-#       }
-#       
-#       else{
-#         voi <- VoI[1]
-#         Comp <- wilcox.test(d[[voi]] ~ d[[Groups]])
-#         w = round(as.numeric(Comp[1]), digits = 2)
-#         p = round(as.numeric(Comp[3]), digits = 4)
-#         dDescr3[i,(length(VoI)+2)] <- paste0('W = ', w, ", p = ", p)
-#       }
-#     }
-#     
-#     
-#     names(dDescr3)[ncol(dDescr3)] <- "Between-subject Comparison"
-#   }
-#   
-#   # Change order from dDescr
-#   dDescr <- arrange(dDescr, Group, Variable)
-#   
-#   Output <- list(dDescr, dDescr3)
-#   return(Output)
-# }
