f33b3d0960d958fc820e8a6c059a07e18bc7a467
FWyckmans <fwyckmans@gmail.com>
2021-12-03 09:04:04 +0100

    More regression on other parameters

---
+++ Analyse/RegressionB2.R
@@ -0,0 +1,238 @@
+remove(list = ls())
+
+############################################ Parameter ############################################
+source("MBMFStudy2_Initialization.R")
+Datapath = "Raw_Data/"
+Output_path = "Output/"
+Graphic_path = "Graphics/Graph/"
+
+############################################# Frame ###############################################
+d <- read.delim(paste0(Output_path,"dOKGamFE_Comp7P_OK_HCPG.txt"))
+boxplot(d$dCortiM)
+# d <- filter(d, dCortiM < 1)
+# d$dCortiM <- log(d$dCortiM + 1)
+boxplot(d$dCortiM)
+PrepForReg()
+
+########################################### Regressions ###########################################
+######### No Outliers Removal
+DF = d
+##### Grp Effect analyses
+m1 <- lm(zbeta2 ~ zdCortiM*SampleC, data = DF)
+summary(m1)
+
+interact_plot(m1, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              x.label = "Cortisol elevetion (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "DG",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Grp.tiff"), dpi = 300)
+
+##### OSPAN effect analyses
+mO <- lm(zbeta2 ~ zdCortiM*zOSPAN, data = DF)
+summary(mO)
+
+interact_plot(mO, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_OSPAN.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModOSPAN <- lm(zbeta2 ~ zdCortiM*SampleC*zOSPAN, data = DF)
+mTMedOSPAN <- lm(zbeta2 ~ zdCortiM + SampleC + zOSPAN + zdCortiM:SampleC + zdCortiM:zOSPAN + zOSPAN:SampleC,
+                 data = DF)
+summary(mTModOSPAN)
+summary(mTMedOSPAN)
+
+interact_plot(mTModOSPAN, pred = zdCortiM, modx = zOSPAN, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Mod_OSPANxSampleC.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_SampleCxOSPAN.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_OSPANxSampleC.tiff"), dpi = 300)
+
+anova(mO, mTMed)
+
+##### Raven effect analyses
+mR <- lm(zbeta2 ~ zdCortiM*zRaven, data = DF)
+summary(mR)
+
+interact_plot(mR, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Raven.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModRaven <- lm(zbeta2 ~ zdCortiM*SampleC*zRaven, data = DF)
+mTMedRaven <- lm(zbeta2 ~ zdCortiM + SampleC + zRaven + zdCortiM:SampleC + zdCortiM:zRaven + zRaven:SampleC,
+                 data = DF)
+summary(mTModRaven)
+summary(mTMedRaven)
+
+interact_plot(mTModRaven, pred = zdCortiM, modx = zRaven, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Mod_RavenxSampleC.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_SampleCxRaven.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_RavenxSampleC.tiff"), dpi = 300)
+
+##### With Outliers Removal
+DF = dNoO
+##### Grp Effect analyses
+m1 <- lm(zbeta2 ~ zdCortiM*SampleC, data = DF)
+summary(m1)
+
+interact_plot(m1, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              x.label = "Cortisol elevetion (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "DG",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Grp_NoO.tiff"), dpi = 300)
+
+##### OSPAN effect analyses
+mO <- lm(zbeta2 ~ zdCortiM*zOSPAN, data = DF)
+summary(mO)
+
+interact_plot(mO, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_OSPAN_NoO.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModOSPAN <- lm(zbeta2 ~ zdCortiM*SampleC*zOSPAN, data = DF)
+mTMedOSPAN <- lm(zbeta2 ~ zdCortiM + SampleC + zOSPAN + zdCortiM:SampleC + zdCortiM:zOSPAN + zOSPAN:SampleC,
+                 data = DF)
+summary(mTModOSPAN)
+summary(mTMedOSPAN)
+
+interact_plot(mTModOSPAN, pred = zdCortiM, modx = zOSPAN, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Mod_OSPANxSampleC_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_SampleCxOSPAN_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_OSPANxSampleC_NoO.tiff"), dpi = 300)
+
+anova(mO, mTMed)
+
+##### Raven effect analyses
+mR <- lm(zbeta2 ~ zdCortiM*zRaven, data = DF)
+summary(mR)
+
+interact_plot(mR, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Raven_NoO.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModRaven <- lm(zbeta2 ~ zdCortiM*SampleC*zRaven, data = DF)
+mTMedRaven <- lm(zbeta2 ~ zdCortiM + SampleC + zRaven + zdCortiM:SampleC + zdCortiM:zRaven + zRaven:SampleC,
+                 data = DF)
+summary(mTModRaven)
+summary(mTMedRaven)
+
+interact_plot(mTModRaven, pred = zdCortiM, modx = zRaven, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Mod_RavenxSampleC_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_SampleCxRaven_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "beta2-parameter (z-score)",
+              legend.main = "Raven Score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_beta2_Med_RavenxSampleC_NoO.tiff"), dpi = 300)
+
+anova(mR, mTMedRaven)
---
+++ Analyse/RegressionMBv.R
@@ -0,0 +1,238 @@
+remove(list = ls())
+
+############################################ Parameter ############################################
+source("MBMFStudy2_Initialization.R")
+Datapath = "Raw_Data/"
+Output_path = "Output/"
+Graphic_path = "Graphics/Graph/"
+
+############################################# Frame ###############################################
+d <- read.delim(paste0(Output_path,"dOKGamFE_Comp7P_OK_HCPG.txt"))
+boxplot(d$dCortiM)
+# d <- filter(d, dCortiM < 1)
+# d$dCortiM <- log(d$dCortiM + 1)
+boxplot(d$dCortiM)
+PrepForReg()
+
+########################################### Regressions ###########################################
+######### No Outliers Removal
+DF = d
+##### Grp Effect analyses
+m1 <- lm(zMBv ~ zdCortiM*SampleC, data = DF)
+summary(m1)
+
+interact_plot(m1, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              x.label = "Cortisol elevetion (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "DG",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Grp.tiff"), dpi = 300)
+
+##### OSPAN effect analyses
+mO <- lm(zMBv ~ zdCortiM*zOSPAN, data = DF)
+summary(mO)
+
+interact_plot(mO, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_OSPAN.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModOSPAN <- lm(zMBv ~ zdCortiM*SampleC*zOSPAN, data = DF)
+mTMedOSPAN <- lm(zMBv ~ zdCortiM + SampleC + zOSPAN + zdCortiM:SampleC + zdCortiM:zOSPAN + zOSPAN:SampleC,
+                 data = DF)
+summary(mTModOSPAN)
+summary(mTMedOSPAN)
+
+interact_plot(mTModOSPAN, pred = zdCortiM, modx = zOSPAN, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Mod_OSPANxSampleC.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_SampleCxOSPAN.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_OSPANxSampleC.tiff"), dpi = 300)
+
+anova(mO, mTMed)
+
+##### Raven effect analyses
+mR <- lm(zMBv ~ zdCortiM*zRaven, data = DF)
+summary(mR)
+
+interact_plot(mR, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Raven.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModRaven <- lm(zMBv ~ zdCortiM*SampleC*zRaven, data = DF)
+mTMedRaven <- lm(zMBv ~ zdCortiM + SampleC + zRaven + zdCortiM:SampleC + zdCortiM:zRaven + zRaven:SampleC,
+                 data = DF)
+summary(mTModRaven)
+summary(mTMedRaven)
+
+interact_plot(mTModRaven, pred = zdCortiM, modx = zRaven, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Mod_RavenxSampleC.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_SampleCxRaven.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_RavenxSampleC.tiff"), dpi = 300)
+
+##### With Outliers Removal
+DF = dNoO
+##### Grp Effect analyses
+m1 <- lm(zMBv ~ zdCortiM*SampleC, data = DF)
+summary(m1)
+
+interact_plot(m1, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              x.label = "Cortisol elevetion (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "DG",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Grp_NoO.tiff"), dpi = 300)
+
+##### OSPAN effect analyses
+mO <- lm(zMBv ~ zdCortiM*zOSPAN, data = DF)
+summary(mO)
+
+interact_plot(mO, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_OSPAN_NoO.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModOSPAN <- lm(zMBv ~ zdCortiM*SampleC*zOSPAN, data = DF)
+mTMedOSPAN <- lm(zMBv ~ zdCortiM + SampleC + zOSPAN + zdCortiM:SampleC + zdCortiM:zOSPAN + zOSPAN:SampleC,
+                 data = DF)
+summary(mTModOSPAN)
+summary(mTMedOSPAN)
+
+interact_plot(mTModOSPAN, pred = zdCortiM, modx = zOSPAN, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Mod_OSPANxSampleC_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_SampleCxOSPAN_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedOSPAN, pred = zdCortiM, modx = zOSPAN, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "WM score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_OSPANxSampleC_NoO.tiff"), dpi = 300)
+
+anova(mO, mTMed)
+
+##### Raven effect analyses
+mR <- lm(zMBv ~ zdCortiM*zRaven, data = DF)
+summary(mR)
+
+interact_plot(mR, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Raven_NoO.tiff"), dpi = 300)
+
+##### Moderation-Mediation
+mTModRaven <- lm(zMBv ~ zdCortiM*SampleC*zRaven, data = DF)
+mTMedRaven <- lm(zMBv ~ zdCortiM + SampleC + zRaven + zdCortiM:SampleC + zdCortiM:zRaven + zRaven:SampleC,
+                 data = DF)
+summary(mTModRaven)
+summary(mTMedRaven)
+
+interact_plot(mTModRaven, pred = zdCortiM, modx = zRaven, mod2 = SampleC, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Raven score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Mod_RavenxSampleC_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = SampleC, plot.points = T,
+              interval = T,
+              # modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Grp",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_SampleCxRaven_NoO.tiff"), dpi = 300)
+
+interact_plot(mTMedRaven, pred = zdCortiM, modx = zRaven, plot.points = T,
+              interval = T,
+              modx.values = "plus-minus",
+              x.label = "dCort (z-score)",
+              y.label = "MB-parameter (z-score)",
+              legend.main = "Raven Score",
+              colors = "Qual1")
+ggsave(paste0(Graphic_path, "InteractPlot_MBv_Med_RavenxSampleC_NoO.tiff"), dpi = 300)
+
+anova(mR, mTMedRaven)
